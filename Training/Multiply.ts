d6e83ab020fa52bfd9c0ecda52419f2ea4f33e0e
Dmitry48 <Dima_1996.33@mail.ru>
2023-08-28 13:17:59 +0700

    multiply

---
+++ Training/Multiply.ts
@@ -0,0 +1,11 @@
+
+//This code does not execute properly. Try to figure out why.
+
+export function multiply(a: number, b: number): number {
+    return a + b;
+}
+
+//Решение
+export function multiply(a: number, b: number): number {
+    return a * b;
+}
